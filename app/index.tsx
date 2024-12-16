bf4bb3c1548894d2f09e05dc554c1ddbc9543c60
kys0017 <kys0017@gmail.com>
2024-12-16 20:08:15 +0900

    ✨ 5.5 Reading Objects - fetch list using useQuery

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,13 @@ import styled from 'styled-components/native';
 import { router } from 'expo-router';
 import colors from '@/colors';
 import { Ionicons } from '@expo/vector-icons';
+import { useQuery } from '@realm/react';
+import Feeling from '@/schema/feeling';
+import { FlatList } from 'react-native';
 
 const View = styled.View`
   flex: 1;
-  padding: 100px 0 0 30px;
+  padding: 100px 30px 0 30px;
   background-color: ${colors.bgColor};
 `;
 const Title = styled.Text`
@@ -28,11 +31,43 @@ const Btn = styled.TouchableOpacity`
 const BtnText = styled.Text`
   color: white;
 `;
+const Record = styled.View`
+  background-color: ${colors.cardColor};
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const Emotion = styled.Text`
+  font-size: 24px;
+  margin-right: 10px;
+`;
+const Message = styled.Text`
+  font-size: 18px;
+`;
+const Separator = styled.View`
+  height: 10px;
+`;
 
 export default function Home() {
+  const feelings = useQuery(Feeling);
+  // const happy = feelings.filtered('emotion == "-"');
+
   return (
     <View>
       <Title>My journal</Title>
+      <FlatList
+        data={feelings}
+        contentContainerStyle={{ paddingVertical: 10 }}
+        ItemSeparatorComponent={Separator}
+        keyExtractor={feeling => feeling._id + ''}
+        renderItem={({ item }: { item: Feeling }) => (
+          <Record>
+            <Emotion>{item.emotion}</Emotion>
+            <Message>{item.message}</Message>
+          </Record>
+        )}
+      />
       <Btn onPress={() => router.navigate('/write')}>
         <Ionicons name={'add'} color="white" size={40} />
       </Btn>
